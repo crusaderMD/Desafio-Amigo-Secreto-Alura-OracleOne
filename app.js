d905daa41fad531fb074cf297e8281de947f0775
Luiz R. Bortolotti Jr. <luiz_bortolotti@hotmial.com>
2025-01-21 15:33:15 -0300

    Adicionando comentários ao código

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+// Array para receber os nomes adicionados
 let amigos = [];
 
+// Adiciona o valor do input ao array caso não seja um valor "vazio"
 function adicionarAmigo(){
     let nomeDoAmigoInserido = document.getElementById('amigo').value;
     
@@ -15,11 +17,13 @@ function adicionarAmigo(){
     mostrarNomesDosAmigos();
 }
 
+// Limpa o campo de input
 function limparNomeDoAmigoInserido(){
     let nomeInserido = document.querySelector('input');
     nomeInserido.value = '';
 }
 
+// Adiciona os nomes do array na lista de nomes
 function mostrarNomesDosAmigos(){
     let listaAmigos = document.getElementById('listaAmigos');
 
@@ -34,6 +38,7 @@ function mostrarNomesDosAmigos(){
     listaAmigos.innerHTML = conteudo;
 }
 
+// Sorteia aleatóriamente um nome no array de nomes
 function sortearAmigo(){
 
     let indiceAleatorio;
@@ -51,11 +56,13 @@ function sortearAmigo(){
     mostrarAmigoSorteado(amigoSorteado);
 }
 
+// Limpa a lista de amigos
 function limparListaDeAmigos(){
     let listaDeAmigos = document.getElementById('listaAmigos');
     listaDeAmigos.innerHTML = "";
 }
 
+// Mostra o nome sorteado
 function mostrarAmigoSorteado(nome){
     let nomeSorteado = document.getElementById('resultado');
     nomeSorteado.innerHTML = `<li>O amigo secreto sorteado é: ${nome}</li>`;
