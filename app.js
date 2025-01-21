4f522c1a25d57b107b8d6c4cf605b7384765bbd4
Luiz R. Bortolotti Jr. <luiz_bortolotti@hotmial.com>
2025-01-21 12:58:59 -0300

    Criando função para listar amigos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let amigos = [];
 
 function adicionarAmigo(){
-    let nomeDoAmigoInserido = document.getElementById("amigo").value;
+    let nomeDoAmigoInserido = document.getElementById('amigo').value;
     
     if (nomeDoAmigoInserido.trim() == ""){
         alert('Por favor, insira um nome.')
@@ -9,10 +9,26 @@ function adicionarAmigo(){
     else{
         amigos.push(nomeDoAmigoInserido);        
     }
-    limparNomeDoAmigoInserido();    
+
+    limparNomeDoAmigoInserido();
+    
+    mostrarNomesAmigos();
+
 }
 
 function limparNomeDoAmigoInserido(){
     nomeDoAmigoInserido = document.querySelector('input');
     nomeDoAmigoInserido.value = '';
+}
+
+function mostrarNomesAmigos(){
+    let listaAmigos = document.getElementById('listaAmigos');
+
+    let conteudo = '';
+
+    for (let i = 0; i < amigos.length; i++){
+        conteudo += "<li><strong>" + amigos[i] + "</strong></li>";
+    }
+
+    listaAmigos.innerHTML = conteudo;
 }
