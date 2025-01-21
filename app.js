e40d863fadab1ea729e853f220a7a9f6bab49217
Luiz R. Bortolotti Jr. <luiz_bortolotti@hotmial.com>
2025-01-21 12:36:32 -0300

    Criando funções para adicionar nome e limpar campo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+    let nomeDoAmigoInserido = document.getElementById("amigo").value;
+    
+    if (nomeDoAmigoInserido.trim() == ""){
+        alert('Por favor, insira um nome.')
+    }
+    else{
+        amigos.push(nomeDoAmigoInserido);        
+    }
+    limparNomeDoAmigoInserido();    
+}
+
+function limparNomeDoAmigoInserido(){
+    nomeDoAmigoInserido = document.querySelector('input');
+    nomeDoAmigoInserido.value = '';
+}
